c56a0cd015e52186f8050516f7f1d7426404826d
Ethan Harsh <ethan@ethanharsh.com>
2021-06-28 18:49:40 +0000

    init

---
+++ index.js
@@ -0,0 +1,19 @@
+// TODO: Include packages needed for this application
+const fs = require('fs');
+const inq = require('inquirer');
+// TODO: Create an array of questions for user input
+const questions = [
+    ''
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {
+    console.log(`Welcome to my little ol' README generator.`);
+    console.log(`I hope you enjoy making super awesome READMEs with it 😁👍`);
+}
+
+// Function call to initialize app
+init();
